d9dcaa69b87b785909859a7b4817104642991862
ahmetsuat67 <93923638+ahmetsuat67@users.noreply.github.com>
2024-10-26 14:06:03 +0300

    Update cypress.config.js and home.cy.js

--- cypress-starter - Inclass/cypress/e2e/home.cy.js
+++ cypress-starter - Inclass/cypress/e2e/home.cy.js
@@ -5,11 +5,19 @@ describe('home test', () => {
     })
     it('home', () => {
         cy.login()
-        cy.get('[data-test="inputHeader"]').should('be.visible').contains('Recipe')
-        cy.get('[data-test="loginName"]').should('be.visible').type('Anthony')
-        cy.get('[data-test="loginPassword"]').should('be.visible').type('Clarusway')
-        cy.get('[data-test="loginSbmt"]').should('be.visible').click()
-        cy.url().should('include', '/home')
+        cy.get("[data-test='homeAbout']").contains("About").should("be.visible").click({ force: true });
+        cy.url().should("include", "/about");
+        cy.get("[data-test='homeLogo']").contains("<Anthony/>").should("be.visible").click({ force: true });
+        cy.url().should("include", "/home");
+        cy.get("[data-test='recipeHeader']").contains("Welcome to my").should("be.visible")
+        cy.get("[data-test='recipeSubHeader']").contains("Food App").should("be.visible")
+        cy.get("[data-test='homeSearch']").type("r")      
+        cy.get("[data-test='homeSearchBtn']").click({ force: true });
+        cy.get("[data-test='cardHeader']").should("be.visible")
+        cy.get("[data-test='cardImage']").should("be.visible")
+        cy.get("[data-test='cardBtn']").should("be.visible").first().click({ force: true });
+        cy.url().should("include", "/details");
+       
     })
     
   })
